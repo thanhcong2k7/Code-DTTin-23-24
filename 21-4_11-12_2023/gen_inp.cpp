6501f33c01fd55d10e8a2fac0c5e1c43037b191b
KamyzZ <61042760+thanhcong2k7@users.noreply.github.com>
2023-11-25 04:30:00 -0500

    25/11/2023_2

--- 21-4_11-12_2023/gen_inp.cpp
+++ 21-4_11-12_2023/gen_inp.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    freopen("input","w",stdout);
-    cout << " 100000";
-    for (int i=0; i<100000; i++) cout << " 1 1000000000";
+    freopen("input.txt","w",stdout);
+    cout << " 1000000";
+    for (int i=0; i<1000000; i++) cout << " 1000000000";
     return 0;
 }
