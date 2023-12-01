a2ab72ee1bcc0808aafe7dc0618bc06f7f86d4bc
KamyzZ <61042760+thanhcong2k7@users.noreply.github.com>
2023-12-01 15:54:56 +0700

    commit 2 1/12/2023

--- 21-4_11-12_2023/HSGTinh10_HieuNhoNhat.cpp
+++ 21-4_11-12_2023/HSGTinh10_HieuNhoNhat.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 bool dd[500][500],a[500][500],n;
 int minn=10000010,mn=10000000,mx=0;
+queue<pair<int,int>> a;
+/*
 int solve(int x, int y){
     if (dd[x][y]==false)
         return -1;
@@ -17,6 +19,25 @@ int solve(int x, int y){
             else if (solve(x,y-1))
         }
     }
+}*/
+int solve(){
+    //lấy điểm đã trong hàng đợi từ trước
+    pair<int,int> tmp;
+    a.push({1,1});
+    while(!a.empty()){
+        tmp=a.front();
+        dd[tmp.first,tmp.second]=false; //đánh dấu đường đi
+        a.pop();
+        //queue mấy điểm sau đấy
+        if(dd[tmp.first-1,tmp.second] && tmp.first)
+            a.push({tmp.first-1,tmp.second});
+        if(dd[tmp.first+1,tmp.second])
+            a.push({tmp.first+1,tmp.second});
+        if(dd[tmp.first,tmp.second-1])
+            a.push({tmp.first,tmp.second-1});
+        if(dd[tmp.first-1,tmp.second+1])
+            a.push({tmp.first-1,tmp.second+1});
+    }
 }
 int main()
 {
@@ -28,10 +49,10 @@ int main()
             cin >> a[i][j];
     for (int i=0; i<=n+1; i++)
         for (int j=0; j<=n+1; j++)
-            chk[i][j]=false;
+            dd[i][j]=false;
     for (int i=1; i<=n; i++)
         for (int j=1; j<=n; j++)
-            chk[i][j]=true;
-    chk[1][1]=false;
-    cout << solve(1,1) << endl;
+            dd[i][j]=true;
+    dd[1][1]=false;
+    cout << solve() << endl;
 }
